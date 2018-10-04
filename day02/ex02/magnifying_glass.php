31ac299869784bf08ea8e8eeda2d9ab9e40a9873
Alex van Niekerk <vanniekerk.alex@gmail.com>
2018-10-04 17:01:19 +0200

    Day 02

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -1,8 +1,34 @@
 #!/usr/bin/php
 <?PHP
-if ($argc > 1)
-{
-    //$fd = fopen(trim($argv[1]), "r");
-    print_r($argv);
+if ($argc > 1) {
+	$line = "";
+    $fd = fopen("./".trim($argv[1]), "r");
+	while (!feof($fd)) 
+	{
+		$line .= fgets($fd);
+	}
+        $line = preg_replace_callback(
+            '/(<a)(.*?)(>)(.*)(<\/a>)/',
+            function ($matches) {
+				{
+					
+					$matches[0] = preg_replace_callback('/(")(.*?)(.*?)(<\/a>)/',
+					function ($matches) 
+					{
+						return strtoupper($matches[0]);
+					}
+					, $matches[0]);
+
+					$matches[0] = preg_replace_callback('/(>)(.*?)(<.*?)/',
+					function ($matches) {
+						return strtoupper($matches[0]);
+					}
+					, $matches[0]);
+
+					return ($matches[0]);
+				}
+            }, $line);
+        echo $line;
+	fclose($fd);
 }
 ?>
